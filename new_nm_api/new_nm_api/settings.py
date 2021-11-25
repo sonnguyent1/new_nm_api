9ee25e650b55b550ae3a8161e16d61a2038b26b4
Son Nguyen <sonnguyentruong87@gmail.com>
2021-11-25 21:34:43 +0700

    added in simple plain page.

--- new_nm_api/new_nm_api/settings.py
+++ new_nm_api/new_nm_api/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'new_nm_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
