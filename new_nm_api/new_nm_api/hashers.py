bf407761e5045aaac6d5a2a2642468556d993372
Son Nguyen <sonnguyentruong87@gmail.com>
2021-11-29 21:45:28 +0700

    added in debug string

--- new_nm_api/new_nm_api/hashers.py
+++ new_nm_api/new_nm_api/hashers.py
@@ -8,5 +8,6 @@ class NewNMMD5PasswordHasher(MD5PasswordHasher):
     def encode(self, password, salt):
             assert password is not None
             assert salt and '$' not in salt
+            print('reached here !!!!!!!!!!!')
             hash = hashlib.md5(force_bytes(password + salt)).hexdigest()
             return "%s$%s$%s" % (self.algorithm, salt, hash)
