eb2cb23148d9f8a6c26b377d64503dc66202dabf
Son Nguyen <sonnguyentruong87@gmail.com>
2024-06-19 22:25:31 +0700

    url pattern

--- new_nm_api/new_nm_api/api/urls.py
+++ new_nm_api/new_nm_api/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls.conf import re_path
-from .views.tv_presentation import presentations_details, presentations
+from .views.tv_presentation import presentations_details, presentations, share_folder_presentation_members
 
 urlpatterns = [ 
     re_path('^$', presentations),
     re_path('^(?P<pk>\d+)/?$', presentations_details),
+    re_path('^(?P<pk>\d+)/sharefolder/?$', share_folder_presentation_members),
 ]
